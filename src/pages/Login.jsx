5f7d593ab95a79b586edd3baf526458ae08a55dc
Kamary <gillespiekamary@gmail.com>
2023-04-28 13:35:02 -0600

    test

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -25,13 +25,16 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch(`/api/users/login`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ email, password: pass }),
-      });
+      const response = await fetch(
+        `${process.env.HEROKU_APP}/api/users/login`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email, password: pass }),
+        }
+      );
 
       if (!response.ok) {
         throw new Error("Login failed");
