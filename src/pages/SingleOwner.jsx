539229e2c4cfc7d469447f280ed1fdcad9d7b55d
Kamary <gillespiekamary@gmail.com>
2023-04-28 16:18:49 -0600

    test

--- src/pages/SingleOwner.jsx
+++ src/pages/SingleOwner.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Row, Col } from "react-bootstrap";
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 import UpdateOwner from "../components/UpdateOwner";
 import ProfileCard from "../components/ProfileCard";
@@ -7,6 +9,25 @@ import OwnerLandHoldings from "../components/OwnerLandHoldings";
 import NavBar from "../components/NavBar";
 
 const SingleOwner = () => {
+  const { ownerId } = useParams();
+  const [owner, setOwner] = useState(null);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    fetch(` https://obscure-bayou-28121.herokuapp.com/api/owners/${ownerId}`)
+      .then((response) => response.json())
+      .then((owner) => {
+        setOwner(owner);
+      })
+      .catch((error) => {
+        setError(error);
+      });
+  }, [ownerId]);
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+  if (!owner) {
+    return <div>Loading...</div>;
+  }
   return (
     <div>
       <NavBar />
